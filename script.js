4d3644b3884510bbf3618f53150450ea3db11f30
Rick Sanchez <matinbeigi99@gmail.com>
2024-12-29 22:15:06 +0330

    update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('imageUpload').addEventListener('change', function(event
         };
         reader.readAsDataURL(file);
     } else {
-        defaultImage.src = 'IMG_20241229_141017_321~2.jpg';
+        defaultImage.src = 'default-image-yektanet.jpg';
     }
 });
 
